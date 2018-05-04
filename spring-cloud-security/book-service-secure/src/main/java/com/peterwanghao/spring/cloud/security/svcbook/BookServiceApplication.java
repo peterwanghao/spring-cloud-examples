36d146463a808ae1198197527f32abab32cc4b26
wanghao <2448408127@qq.com>
2018-05-04 14:34:40 +0800

    Zipkin服务跟踪添加引用

--- spring-cloud-security/book-service-secure/src/main/java/com/peterwanghao/spring/cloud/security/svcbook/BookServiceApplication.java
+++ spring-cloud-security/book-service-secure/src/main/java/com/peterwanghao/spring/cloud/security/svcbook/BookServiceApplication.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookServiceApplication {
+    
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
--- spring-cloud-security/gateway-secure/src/main/java/com/peterwanghao/spring/cloud/security/gateway/SecurityConfig.java
+++ spring-cloud-security/gateway-secure/src/main/java/com/peterwanghao/spring/cloud/security/gateway/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.data.redis.RedisFlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**   
  * @ClassName:  SecurityConfig
